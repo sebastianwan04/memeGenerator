ab30aa3acf92051ceef1e60cba5aa8ed920dfeca
sebastian.wan04 <sebastian.wan04@gmail.com>
2022-05-26 11:54:22 +0200

    Little refactor, changed meme to become an object

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,26 @@
 import React from "react";
+import memesData from "../memeData.js"
 
 export default function Main(){
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImg: ""
+    })
+
+    const [allMemeImage, setAllMemeImage] = React.useState(memesData)
+
+    function getMemeImage(){
+        const memesArray =  allMemeImage.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImg: url
+        })); 
+    }
+
     return(
         <main>
             <form action="" className="form">
@@ -16,10 +36,11 @@ export default function Main(){
                         className="form--input"
                         />
                 </div>
-                <button className="form--button">
+                <button type="button" className="form--button" onClick={getMemeImage}>
                 Get a new meme image 🖼
                 </button>
             </form>
+            <img src={meme.randomImg} className="meme--image" />
         </main>
     )
 }
